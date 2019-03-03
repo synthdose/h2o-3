2314dbf0ae6c73571addc7f0769288e7a6604d4a
Jan Sterba <jan.sterba@h2o.ai>
2019-03-03 18:25:03 +0100

    WIP

--- docker/hadoop/hdp/scripts/sbin/install_hive.sh
+++ docker/hadoop/hdp/scripts/sbin/install_hive.sh
@@ -2,18 +2,9 @@
 
 set -x -e
 
-if [ -z ${HIVE_VERSION} ]; then
-    echo "HIVE_VERSION must be set"
-    exit 1
-fi
 
-# Download and initialize HIVE
-cd /home/hive
-wget http://archive.apache.org/dist/hive/hive-${HIVE_VERSION}/apache-hive-${HIVE_VERSION}-bin.tar.gz
-tar -xzf apache-hive-${HIVE_VERSION}-bin.tar.gz
-rm apache-hive-${HIVE_VERSION}-bin.tar.gz
-chown -R hive:hive apache-hive-*/
+apt-get install -y hive hive-server2
 
-find $HIVE_HOME/lib | grep '.jar' | \
+find /usr/hdp/current/hive-client/lib | grep '.jar' | \
     grep -E 'hive-jdbc|hive-common|hive-exec|hive-service|libfb303|libthrift' | \
     tr '\n' ':' > /opt/hive-jdbc-cp
